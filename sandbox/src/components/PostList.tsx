b54c1cfff48f635761c93e20ade1e1a3c5856d9a
Melvyn Andrew <melvynandrew99@gmail.com>
2023-12-29 01:27:40 -0500

    Added PostList component

---
+++ sandbox/src/components/PostList.tsx
@@ -0,0 +1,37 @@
+import Post from "./Post";
+import { PostProps } from "./Post";
+import styled from 'styled-components';
+
+// Styled Components allows us to embed CSS into the tsx files like we do with VUE
+const StyledList = styled.ul`
+	list-style: none;
+	max-width: 50rem;
+	margin: 1rem auto;
+	padding: 1rem 0;
+	display: grid;
+	gap: 1rem;
+	grid-template-columns: repeat(3, 30%);
+	justify-content: center;
+`;
+
+// This will serve as the temporary state for now
+const allPosts: PostProps[] = [
+	{ author: "melvyn", body: "hey its me" },
+	{ author: "bass guy", body: "I love bass" },
+	{ author: "melvyn", body: "React is easy!" }
+]
+
+function PostList() {
+	return (
+		<StyledList>
+			{allPosts.map((p, i) => (
+				<li key={i}>
+					<Post author={p.author} body={p.body}/>
+				</li>
+			))}
+		</StyledList>
+	)
+
+}
+
+export default PostList;
