86cc6d513a5422b75242eb4dd82266fffb3d81cd
Phil Smeby <phillip.smeby@hill-rom.com>
2023-12-28 23:59:23 -0500

    added first component

---
+++ sandbox/src/components/Post.tsx
@@ -0,0 +1,9 @@
+function Post() {
+	return <div>
+		<p>Phil</p>
+		<p>React with Typescript is so groovy!</p>
+	</div>
+}
+
+// Note to self, that this allows you to name the imported code as anything you want.
+export default Post;
