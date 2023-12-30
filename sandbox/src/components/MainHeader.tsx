03e4256c36bc9169932dc81cd8aca24609465adc
Melvyn Andrew <melvynandrew99@gmail.com>
2023-12-30 03:09:42 -0500

    added header scaffolding

---
+++ sandbox/src/components/MainHeader.tsx
@@ -0,0 +1,13 @@
+import { MdPostAdd, MdMessage } from 'react-icons/md';
+
+import styles from './MainHeader.module.css';
+
+function MainHeader() {
+	return (
+		<header>
+
+		</header>
+	)
+}
+
+export default MainHeader;
